5f80e647c966f8c0b8ed56223ac86836b5d0fedc
ARauf Git <umiasad2017@gmail.com>
2020-05-04 03:41:31 -0400

    add AddTransaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const TransactionList = () => {
     return (
-        <div>
-                             
-        </div>
+        <>
+          <h3>History</h3>
+          <ul className="list">
+              <li className="minus">
+                  Cash <span>-$400</span><button className="delete-btn">x</button>
+              </li>
+              </ul>                   
+        </>
     )
 }
